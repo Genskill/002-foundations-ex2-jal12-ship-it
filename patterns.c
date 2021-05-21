69f17606ccd05836a4a4e3d8c46f1b0aafaab6e1
ceccs18c26 <jalena.mathew.31@gmail.com>
2021-05-21 09:39:26 +0530

    Exercise 2

---
+++ patterns.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<cs50.h>
+
+void pattern( int, int, char, char);
+
+int main(void)
+{
+	int N, R;
+	char c, f;
+	N = get_int("Enter type of pattern (1 or 2) ");
+	R = get_int("Number of rows ");
+	if (N == 1)
+		pattern(N, R+1, c='#', f='.');
+	else
+		pattern(N, R, c=' ', f='#');
+}
+		
+		
+void pattern(int N, int R, char c, char f)
+{
+	int j;
+	for( int i = 0; i < R; i++)
+	{		
+		for(j = 0; j < R-1-i; j++)
+			printf("%c", c);
+		if( N == 2)	
+			for(int k = j; k < R; k++)	
+				printf("%c", f);
+		printf("\n");
+	}
+}	
+	
